6b0dfb4f708a643bcc73e28a253a6d5b2e90f257
Faizah-Hazeez <hazeezolakiitan@gmail.com>
2025-05-05 23:53:45 +0100

    api-supabase-filtering

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -2,14 +2,13 @@ import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
-import Spinner from "../../ui/Spinner";
 import { useBookings } from "./useBookings";
+import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
   const { bookings, isLoading } = useBookings();
 
   if (isLoading) return <Spinner />;
-
   if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
@@ -24,12 +23,12 @@ function BookingTable() {
           <div></div>
         </Table.Header>
 
-        {/* <Table.Body
+        <Table.Body
           data={bookings}
           render={(booking) => (
             <BookingRow key={booking.id} booking={booking} />
           )}
-        /> */}
+        />
       </Table>
     </Menus>
   );
--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,13 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter = !filterValue || filterValue === "all" ? null : {};
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: "bookings",
+    queryKey: ["bookings"],
     queryFn: getBookings,
   });
 
