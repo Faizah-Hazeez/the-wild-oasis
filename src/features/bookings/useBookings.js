3b71dcaa556b221be33db2879fda77e2974e1af1
Faizah-Hazeez <hazeezolakiitan@gmail.com>
2025-05-08 08:28:12 +0100

    pagination with supabase

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -7,15 +7,28 @@ export function useBookings() {
 
   // FILTER
   const filterValue = searchParams.get("status");
-  const filter = !filterValue || filterValue === "all" ? null : {};
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // : { field: "totalPrice", value: 5000, method: "gte" };
+
+  // SORTING
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, bookings, error };
+  return { isLoading, bookings, error, count };
 }
